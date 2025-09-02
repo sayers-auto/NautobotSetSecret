8cbe30b2665760e9c7603dadff30005b1093b026
sayers-auto <ncore@sayers.com>
2025-09-02 17:23:31 -0400

    indentation change

--- jobs/SetTextSecret.py
+++ jobs/SetTextSecret.py
@@ -1,12 +1,13 @@
-from nautobot.extras.jobs import Job, StringVar, register_jobs
+from nautobot.apps.jobs import Job, StringVar, register_jobs
 import os
 
 class SetTextSecret(Job):
     class Meta:
         name = "Set Text Secret"
         description = "Writes a value to a text file in /opt/nautobot/secrets/[name].txt"
-	secret_name = StringVar(description="Secret Name")
-	secret_value = StringVar(description="Secret Value",secret=True)
+
+    secret_name = StringVar(description="Secret Name")
+    secret_value = StringVar(description="Secret Value", secret=True)
 
     def run(self, *, secret_name, secret_value):
         file_path = f"/opt/nautobot/secrets/{secret_name}.txt"
